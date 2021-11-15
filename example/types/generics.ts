dfe42109488b298d555babd9c422970e430be982
d-kimsuon <d-kimuson@gmail.com>
2021-11-16 01:39:37 +0900

    fix: fix skip generics logic

--- example/types/generics.ts
+++ example/types/generics.ts
@@ -1,9 +1,15 @@
-// should be resolved
+// should be skipped
 export type GenericsWithDefault<T = string> = {
   value: T
 }
 
 // should be skipped
-export type GenericsWithNoDefault<T extends string> = {
+export type GenericsWithNoDefault<T extends string, Y = string> = {
   value: T
 }
+
+// should be resolved
+export type ResultOfGenerics = Partial<{
+  id: number
+  time: Date
+}>
