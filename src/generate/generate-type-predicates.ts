e96e416dd6820592661fdf5efc95db128bd33c8b
Yudai NAKATA <yudai.nkt@gmail.com>
2021-12-04 19:22:18 +0900

    fix: fix format issues without dedent

--- src/generate/generate-type-predicates.ts
+++ src/generate/generate-type-predicates.ts
@@ -247,25 +247,26 @@ export function generateTypePredicates(
     isAssertion: boolean
   }): string => {
     return isAssertion
-      ? `/**
-      * Assert if a variable is of type {@link ${typeName}} and throws a TypeError if the assertion fails.
-      * This function is automatically generated using [type-predicates-generator](https://www.npmjs.com/package/type-predicates-generator).
-      * @param value Argument to inspect.
-      * @throw TypeError if the given argument is not compatible with the type {@link ${typeName}}.
-      */
-      `
-      : `/**
-      * Check if a variable is of type {@link ${typeName}} and narrow it down to that type if the check passes.
-      * This function is automatically generated using [type-predicates-generator](https://www.npmjs.com/package/type-predicates-generator).
-      * @param arg_0 Argument to inspect.
-      ${
-        type.__type === "ArrayTO"
-          ? "* @param checkOpt Whether to check all elements of the array or only the first one."
-          : ""
-      }
-      * @return \`true\` if the argument is of type {@link ${typeName}}, \`false\` otherwise.
-      */
-      `
+      ? `\
+/**
+  * Assert if a variable is of type {@link ${typeName}} and throws a TypeError if the assertion fails.
+  * This function is automatically generated using [type-predicates-generator](https://www.npmjs.com/package/type-predicates-generator).
+  * @param value Argument to inspect.
+  * @throw TypeError if the given argument is not compatible with the type {@link ${typeName}}.
+  */
+`
+      : `\
+/**
+  * Check if a variable is of type {@link ${typeName}} and narrow it down to that type if the check passes.
+  * This function is automatically generated using [type-predicates-generator](https://www.npmjs.com/package/type-predicates-generator).
+  * @param arg_0 Argument to inspect.${
+    type.__type === "ArrayTO"
+      ? "\n  * @param checkOpt Whether to check all elements of the array or only the first one."
+      : ""
+  }
+  * @return \`true\` if the argument is of type {@link ${typeName}}, \`false\` otherwise.
+  */
+`
   }
 
   const generatedTypeNames: string[] = []
@@ -324,7 +325,11 @@ export function generateTypePredicates(
       })};
 ${
   asserts
-    ? `export function assertIs${typeName}(value: unknown): asserts value is ${typeName} {
+    ? `${
+        comment
+          ? generateJSDocComment({ type, typeName, isAssertion: true })
+          : ""
+      }export function assertIs${typeName}(value: unknown): asserts value is ${typeName} {
   if (!is${typeName}(value)) throw new TypeError(\`value must be ${typeName} but received \${value}\`)
 };`
     : ""
